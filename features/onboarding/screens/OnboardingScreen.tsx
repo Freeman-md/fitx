f81b12603656fc477f501105e052d5ff21bc2d7d
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-12-28 20:02:24 +0000

    feat(onboarding): update slide content and add onboarding images

--- features/onboarding/screens/OnboardingScreen.tsx
+++ features/onboarding/screens/OnboardingScreen.tsx
@@ -8,19 +8,19 @@ const slides = [
     id: 'slide-1',
     title: 'Plan',
     description: 'Simple templates, ready to run.',
-    image: require('../../../assets/images/splash-icon.png'),
+    image: require('../../../assets/images/onboarding-1.jpg'),
   },
   {
     id: 'slide-2',
     title: 'Train',
     description: 'Stay focused on the next set.',
-    image: require('../../../assets/images/splash-icon.png'),
+    image: require('../../../assets/images/onboarding-2.jpg'),
   },
   {
     id: 'slide-3',
     title: 'Review',
     description: 'Clear records of what happened.',
-    image: require('../../../assets/images/splash-icon.png'),
+    image: require('../../../assets/images/onboarding-3.jpg'),
   },
 ];
 
